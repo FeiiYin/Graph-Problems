cbd4215371cb8994bd4c08f775309d5b5885f654
Samaritan <1594320575@qq.com>
2018-10-31 17:58:29 +0800

    Update and rename hdu 5760 to hdu 5760 回文上升子序列 最长长度和个数dp.cpp

--- version3.0/hdu 5760 回文上升子序列 最长长度和个数dp.cpp
+++ version3.0/hdu 5760 回文上升子序列 最长长度和个数dp.cpp
@@ -1,9 +1,13 @@
-题意: 给出n个数，要找到一个合法的最长子序列s，输出其长度，并且输出不同的s的个数。s序列必须是回文的，并且中间最小，往两边依次不减。s1与s2不同当且仅当长度不同或者存在某位s1[i]!=s2[i] 
-以dp[l][r]表示从l开始以r结束的最长合法子序列的长度及数量（first表示长度，second表示数量），以nxt[i][j]表示序列中i之后第一个等于j的数的位置，以pre[i][j]表示序列中i之前最后一个等于j的数的位置（求这两个数组需要对a序列离散化），那么显然有一个朴素的O(n^3)的转移： 
+题意: 给出n个数，要找到一个合法的最长子序列s，输出其长度，并且输出不同的s的个数。s序列必须是回文的，并且中间最小，往两边依次不减。
+s1与s2不同当且仅当长度不同或者存在某位s1[i]!=s2[i] 
+以dp[l][r]表示从l开始以r结束的最长合法子序列的长度及数量（first表示长度，second表示数量），以nxt[i][j]表示序列中i之后第一个等于j的数的位置，
+以pre[i][j]表示序列中i之前最后一个等于j的数的位置（求这两个数组需要对a序列离散化），那么显然有一个朴素的O(n^3)的转移： 
 dp[l][r]=max(dp[nxt[l][c]][pre[r][c]]+2)（first+2,second不变） 
 考虑到时间限制，需要优化上述转移，若固定l，那么随着r的右移，dp[l][r]一定是越来越优，用ans临时记录最优解，当r右移时就可以用ans更新dp数组，
 如果a[i]=a[j]则令dp[i][j].first=ans.first+2,dp[i][j].second=ans.second，如果a[i]>=a[j]，则令ii=nxt[i][a[j]]表示找到i右边一个与a[j]值
-相等的位置，如果dp[ii][j].first>ans.first则更新ans，如果两者相同说明和ans相同长度的序列又出现了，但是不能直接将dp[ii][j].second加到ans.second上去，因为还有重复的问题，令jj=pre[j][a[j]]，若dp[ii][jj].first==dp[ii][j].first说明ii~jj的所有最长序列必然都会出现在ii~j中，而ii~jj中的所有最长序列已经累加到ans.second中过，故要将这部分减掉之后再在ans.second上加上dp[ii][j].second，最后枚举最长子序列两端的值即可得到最优解
+相等的位置，如果dp[ii][j].first>ans.first则更新ans，如果两者相同说明和ans相同长度的序列又出现了，但是不能直接将dp[ii][j].second加到ans.second
+上去，因为还有重复的问题，令jj=pre[j][a[j]]，若dp[ii][jj].first==dp[ii][j].first说明ii~jj的所有最长序列必然都会出现在ii~j中，而ii~jj中的所有
+最长序列已经累加到ans.second中过，故要将这部分减掉之后再在ans.second上加上dp[ii][j].second，最后枚举最长子序列两端的值即可得到最优解
 #include<cstdio>
 #include<iostream>
 #include<cstring>
